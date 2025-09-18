0b75ecbf1b5d40886afdfad9d8bea3bb8249d7f7
Pranav Chopade <pranavchopade129@gmail.com>
2025-09-18 21:11:48 +0530

    react-router setup

---
+++ MysticBug/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import NotFound from "./components/NotFound"
+
+const App = () => {
+  return (
+    <div className="max-w-[1440px] mx-auto min-h-screen sm:px-15 sm:py-2.5 ">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
