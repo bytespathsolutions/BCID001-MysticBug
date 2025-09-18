6838f2ffbd66c13e371aa73e6443af605644ae48
Pranav Chopade <pranavchopade129@gmail.com>
2025-09-18 21:13:08 +0530

    add assets

---
+++ MysticBug/src/assets/assets.js
@@ -0,0 +1,30 @@
+import { FaRegClock } from "react-icons/fa";
+import { LuTablet } from "react-icons/lu";
+import { RiChatCheckLine } from "react-icons/ri";
+import { FaHandHoldingHeart } from "react-icons/fa";
+import { FiHome } from "react-icons/fi";
+import { BiBook } from "react-icons/bi";
+import bot from "../assets/bot.png"
+import redLight from "../assets/redLight.png"
+import banner from "../assets/banner.png"
+import logo from "../assets/logo.png"
+import Offer from "../assets/Offer.png"
+import clip_path_group from "../assets/clip_path_group.png"
+import calling_girl from "../assets/calling_girl.png"
+import repair_man from "../assets/repair_man.png"
+import Group1 from "../assets/Group_1.png"
+import homepage from "../assets/homepage.png"
+
+
+
+export const icons = {
+  FaRegClock,
+  LuTablet,
+  RiChatCheckLine,
+  FaHandHoldingHeart,
+  FiHome,
+  BiBook
+}
+export const images = {
+  bot, redLight, banner, Offer, logo, clip_path_group, calling_girl, repair_man, Group1, homepage
+}
