dcfbe7d3891583ab630f1ea4ca2dcc53c3f5bc37
Pranav Chopade <pranavchopade129@gmail.com>
2025-09-25 19:06:35 +0530

    configure tailwind version

---
+++ MysticBug/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
+        'merriweather': ['Merriweather', 'serif'],
+      },
+      fontSize: {
+        '18': '18px',
+        '56': '56px',
+        '20': '20px',
+        '48': '48px',
+        '32': '32px',
+      }
+    },
+  },
+  plugins: [],
+}
