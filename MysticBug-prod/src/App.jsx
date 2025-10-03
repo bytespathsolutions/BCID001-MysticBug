b38ee71f56d2fd63edf1f368ae9d885deaa0526f
Pranav Chopade <pranavchopade129@gmail.com>
2025-10-03 11:26:19 +0530

    add patient login

--- MysticBug-prod/src/App.jsx
+++ MysticBug-prod/src/App.jsx
@@ -4,12 +4,14 @@ import OtpReset from './pages/OtpReset'
 import AdminLogin from './pages/AdminLogin'
 import InvestorLogin from './pages/InvestorLogin'
 import DoctorLogin from './pages/DoctorLogin'
+import PatientLogin from './pages/PatientLogin'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path='/patient-login' element={<PatientLogin />} />
         <Route path='/doctor-login' element={<DoctorLogin />} />
         <Route path='/investor-login' element={<InvestorLogin />} />
         <Route path='/admin-login' element={<AdminLogin />} />
