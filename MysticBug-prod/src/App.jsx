c6e985175ca5f7cb68073b0d3485c4b39d27e9ce
Pranav Chopade <pranavchopade129@gmail.com>
2025-10-03 11:24:20 +0530

    add investor login

--- MysticBug-prod/src/App.jsx
+++ MysticBug-prod/src/App.jsx
@@ -2,12 +2,15 @@ import Login from './pages/Login'
 import { Routes, Route } from "react-router-dom"
 import OtpReset from './pages/OtpReset'
 import AdminLogin from './pages/AdminLogin'
+import InvestorLogin from './pages/InvestorLogin'
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
 
+        <Route path='/investor-login' element={<InvestorLogin />} />
         <Route path='/admin-login' element={<AdminLogin />} />
         <Route path='/forgot-password' element={<OtpReset />} />
 
