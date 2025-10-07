389d2be82141a01b1972d5246ecf7d65ea6571d4
Pranav Chopade <pranavchopade129@gmail.com>
2025-10-07 17:10:16 +0530

    add appointment model

---
+++ MysticBug-prod/Backend/models/Appointment.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const AppointmentSchema = new mongoose.Schema({
+  patientName: {
+    type: String,
+    default: ''
+  },
+  reason: {
+    type: String,
+    required: true
+  },
+  doctor: {
+    type: String,
+    default: ''
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  timeSlot: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export const Appointment = mongoose.model('Appointment', AppointmentSchema);
