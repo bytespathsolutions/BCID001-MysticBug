3e07cfb8192404ac8a8a7552256afbc6db8d546b
Pranav Chopade <pranavchopade129@gmail.com>
2025-10-09 10:54:42 +0530

    fix route

--- MysticBug-prod/Frontend/src/ProtectedRoute.jsx
+++ MysticBug-prod/Frontend/src/ProtectedRoute.jsx
@@ -1,13 +1,14 @@
+import { Navigate } from "react-router-dom";
 import { useAuth } from "./Context/AuthContext";
-import { Navigate } from "react-router-dom"
-
 const ProtectedRoute = ({ children, allowedRole }) => {
   const { user, role } = useAuth();
+
   if (!user) return <Navigate to="/" replace />;
+
   if (role !== allowedRole) {
     return <Navigate to="/unauthorized" replace />;
   }
 
   return children;
 }
-export default ProtectedRoute;
+export default ProtectedRoute
