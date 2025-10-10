770bc1193d4d943ae01f93269905aff8432ad525
Pranav Chopade <pranavchopade129@gmail.com>
2025-10-10 12:56:56 +0530

    add frontend pages assets

--- MysticBug-prod/Frontend/src/assets/assets.js
+++ MysticBug-prod/Frontend/src/assets/assets.js
@@ -20,7 +20,68 @@ import { FaGoogle } from "react-icons/fa";
 import { FaApple } from "react-icons/fa";
 
 
+import { FaRegClock } from "react-icons/fa";
+import { LuTablet } from "react-icons/lu";
+import { RiChatCheckLine } from "react-icons/ri";
+import { FaHandHoldingHeart } from "react-icons/fa";
+import { FiHome } from "react-icons/fi";
+import { BiBook } from "react-icons/bi";
+import bot from "../assets/homepage/bot.png"
+import redLight from "../assets/homepage/redLight.png"
+import DiseaseBanner7 from "../assets/homepage/DiseaseBanner7.png"
+import Offer from "../assets/homepage/Offer.png"
+import clip_path_group from "../assets/homepage/clip_path_group.png"
+import calling_girl from "../assets/homepage/calling_girl.png"
+import repair_man from "../assets/homepage/repair_man.png"
+import Group1 from "../assets/homepage/Group_1.png"
+import homepage from "../assets/homepage/homepage.png"
+import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import calling_1 from "../assets/ContactPage/calling_1.png"
+import { MdOutlineMail } from "react-icons/md";
+import mail from "../assets/ContactPage/mail.png"
+import office from "../assets/ContactPage/office.png"
+import { CiLocationOn } from "react-icons/ci";
+import { IoCallOutline } from "react-icons/io5";
+import mobile from "../assets/ContactPage/mobile.png"
+import Who_are_we from "../assets/Aboutus/Who_are_we.png"
+import clip_path_vertical from "../assets/Aboutus/clip_path_vertical.png"
+import owner from "../assets/Aboutus/owner.png"
+import terms from "../assets/Terms/terms.png"
+import stones_left from "../assets/Terms/stones-left.png"
+import stones_right from "../assets/Terms/stones-right.png"
+import privacyPolicy from "../assets/privacy-policy/privacyPolicy.png"
+import DiseaseBanner01 from "../assets/Disease/DiseaseBanner01.png"
+import DiseaseBanner02 from "../assets/Disease/DiseaseBanner02.png"
+import DiseaseBanner03 from "../assets/Disease/DiseaseBanner03.png"
+import DiseaseBanner04 from "../assets/Disease/DiseaseBanner04.png"
+import DiseaseBanner05 from "../assets/Disease/DiseaseBanner05.png"
+import DiseaseBanner06 from "../assets/Disease/DiseaseBanner06.png"
+import DiseaseBanner07 from "../assets/Disease/DiseaseBanner07.png"
+import DiseaseBanner08 from "../assets/Disease/DiseaseBanner08.png"
+import DiseaseBanner09 from "../assets/Disease/DiseaseBanner09.png"
 
-export const images = { Clippath_group, Patient, Doctor, otp_man, Investor, Group, lower_clip, upper_clip, bottomWave, Appointments, Monitoring, Reminders, Medical_Records, Prescriptions, Medical_assistant, Admin, logo }
+export const images = { Clippath_group, Patient, Doctor, otp_man, Investor, Group, lower_clip, upper_clip, bottomWave, Appointments, Monitoring, Reminders, Medical_Records, Prescriptions, Medical_assistant, Admin, logo, }
 
 export const icons = { FaLeaf, FaGoogle, FaApple }
+
+
+export const default_page_icons = {
+  FaRegClock,
+  LuTablet,
+  RiChatCheckLine,
+  FaHandHoldingHeart,
+  FiHome,
+  BiBook,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+  FaLeaf,
+  MdOutlineMail, CiLocationOn
+  , IoCallOutline
+}
+export const default_page_images = {
+  bot, redLight, DiseaseBanner7, Offer, logo, clip_path_group, calling_girl, repair_man, Group1, homepage
+  ,
+  calling_1, mail, office, mobile, Who_are_we, clip_path_vertical, owner, terms, stones_left, stones_right, privacyPolicy, DiseaseBanner01, DiseaseBanner02, DiseaseBanner03, DiseaseBanner04, DiseaseBanner05, DiseaseBanner06, DiseaseBanner07, DiseaseBanner08, DiseaseBanner09
+}
