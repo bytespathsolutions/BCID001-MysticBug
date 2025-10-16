cd8ba05bf08242380a1dcac5cabca6b0c25e7e03
Pranav Chopade <pranavchopade129@gmail.com>
2025-10-16 17:41:38 +0530

    add feedback route and fix cors

--- MysticBug-prod/Backend/server.js
+++ MysticBug-prod/Backend/server.js
@@ -2,14 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import admin from "firebase-admin";
-import { usersRoutes, appointmentsRoutes, medicalRoutes, patientRoutes, doctorRoutes } from "./routes/users.js";
+import { usersRoutes, appointmentsRoutes, medicalRoutes, patientRoutes, doctorRoutes, feedbackRouter } from "./routes/users.js";
 import { dbConn } from "./config/dbconnection.js";
 
 dotenv.config();
 
 const app = express();
 app.use(
-  cors("http://localhost:5000")
+  cors(
+    {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true
+    }
+  )
 );
 app.use(express.json());
 
@@ -32,6 +39,7 @@ app.use("/api/appointments", appointmentsRoutes);
 app.use("/api/medical_records", medicalRoutes);
 app.use("/api/patient", patientRoutes);
 app.use("/api/doctors", doctorRoutes);
+app.use("/api", feedbackRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
